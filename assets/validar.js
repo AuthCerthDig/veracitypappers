cb267e4905829a0d9f8c6c5803fb6fd82e555542
AuthCerthDig <authcertdig@gmail.com>
2025-12-20 15:06:01 -0300

    Update validar.js

--- assets/validar.js
+++ assets/validar.js
@@ -17,14 +17,14 @@ function validarDocumento() {
 
             if (doc && doc.status === "VALIDO") {
                 mensagem.innerHTML = `
-                    <strong>Documento Original Válido!</strong><br><br>
-                    Esse certificado foi verificado e as informações desse documento são para o CFP ${doc.cfp}.<br><br>
+                    <strong>DOCUMENTO LOCALIZADO COM SUCESSO! (ATIVO)</strong><br><br>
+                     Documento Original. Validação exclusiva para o CPF nº ${doc.cfp}.<br><br>
                     <strong>Tipo:</strong> ${doc.tipo}<br>
                     <strong>Data de Emissão:</strong> ${doc.data}
                 `;
                 mensagem.className = "mensagem sucesso";
             } else {
-                mensagem.textContent = "Documento não encontrado ou inválido.";
+                mensagem.textContent = "Documento não encontrado ou emitidos antes de 01/01/2022 conforme Portaria MEC Nº 554 DE 11 de março 2019..";
                 mensagem.className = "mensagem erro";
             }
         })
